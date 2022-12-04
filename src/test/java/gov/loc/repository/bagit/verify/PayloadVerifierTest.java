dcf1a10b0d89d6966e4179fd4284a1e338844a4e
David Moles <dmoles@dmoles.net>
2022-12-03 16:57:17 -0800

    Add test for accented characters in filename

--- src/test/java/gov/loc/repository/bagit/verify/PayloadVerifierTest.java
+++ src/test/java/gov/loc/repository/bagit/verify/PayloadVerifierTest.java
@@ -14,12 +14,12 @@ import gov.loc.repository.bagit.hash.StandardBagitAlgorithmNameToSupportedAlgori
 import gov.loc.repository.bagit.reader.BagReader;
 
 public class PayloadVerifierTest {
-  
+
   private Path rootDir = Paths.get(new File("src/test/resources/bags/v0_97/bag").toURI());
   private BagReader reader = new BagReader();
-  
+
   private PayloadVerifier sut;
-  
+
   @Before
   public void setup(){
     sut = new PayloadVerifier(new StandardBagitAlgorithmNameToSupportedAlgorithmMapping());
@@ -29,29 +29,36 @@ public class PayloadVerifierTest {
   public void testErrorWhenManifestListFileThatDoesntExist() throws Exception{
     rootDir = Paths.get(new File("src/test/resources/filesInManifestDontExist").toURI());
     Bag bag = reader.read(rootDir);
-    
+
     sut.verifyPayload(bag, true);
   }
-  
+
   @Test(expected=FileNotInManifestException.class)
   public void testErrorWhenFileIsntInManifest() throws Exception{
     rootDir = Paths.get(new File("src/test/resources/filesInPayloadDirAreNotInManifest").toURI());
     Bag bag = reader.read(rootDir);
-    
+
     sut.verifyPayload(bag, true);
   }
-  
+
   @Test
   public void testBagWithTagFilesInPayloadIsValid() throws Exception{
     rootDir = Paths.get(new File("src/test/resources/bags/v0_96/bag-with-tagfiles-in-payload-manifest").toURI());
     Bag bag = reader.read(rootDir);
-    
+
     sut.verifyPayload(bag, true);
   }
-  
+
   @Test(expected=FileNotInManifestException.class)
   public void testNotALlFilesListedInAllManifestsThrowsException() throws Exception{
     Path bagDir = Paths.get(new File("src/test/resources/notAllFilesListedInAllManifestsBag").toURI());
+    Bag bag = reader.read(bagDir);
+    sut.verifyPayload(bag, true);
+  }
+
+  @Test
+  public void testBagWithAccentedCharactersInFilenameIsValid() throws Exception {
+    Path bagDir = Paths.get(new File("src/test/resources/nonLatinCharactersInFilename").toURI());
     Bag bag = reader.read(bagDir);
     sut.verifyPayload(bag, true);
   }
